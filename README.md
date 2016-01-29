c91a112e32332f06157fe3de627a9e6c1508e60e
zbx-sadman <sadman41@ya.ru>
2016-01-29 16:08:43 +0300

    Improved code, add LLD feature and a few new objects

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 ## WSUS Miner 
-This is a little Powershell script for fetch data from WSUS 3.0 Server.
+This is a little Powershell script that fetch metric's values from WSUS 3.0 Server.
 
 Support objects:
 - _Status_ - WSUS Status (Number of Approved/Declined/Expired/etc updates, full/partially/unsuccess updated clients and so);
 - _Info_ - Some WSUS Settings;
 - _synchronizationProcess_ - Synchronization process status;
 - _lastSynchronization_ - Last Synchronization data;
+- _database_ - WSUS Database related info;
+- _configuration_ - WSUS configuration info;
+- _computerGroup_ - Virtual object to taking computer group statistic (keys: _ComputerTargetsWithUpdateErrorsCount_, _ComputerTargetsNeedingUpdatesCount_, _ComputersUpToDateCount_, _ComputerTargetsUnknownCount_).
+
+Zabbix's LLD available to:
+- _computerGroup_ 
 
 How to use:
-- Just add to Zabbix Agent config, which run on WSUS host this string: _UserParameter=wsus.miner[*], powershell -File C:\zabbix\scripts\wsus_miner.ps1 -Action "$1" -Object "$2" -Key "$3"_
+- Just add to Zabbix Agent config, which run on WSUS host this string: _UserParameter=wsus.miner[*], powershell -File C:\zabbix\scripts\wsus_miner.ps1 -Action "$1" -Object "$2" -Key "$3" -Id "$4"_ 
 - Put _wsus_miner.ps1_ to _C:\zabbix\scripts_ dir;
 - Make unsigned .ps1 script executable with _Set-ExecutionPolicy RemoteSigned_;
-- Set Zabbix Agent's _Timeout_ to more that 3 sec (may be 10 or 30);
+- Set Zabbix Agent's / Server's _Timeout_ to more that 3 sec (may be 10 or 30);
 - Import [template](https://github.com/zbx-sadman/wsus_miner/tree/master/Zabbix_Templates) to Zabbix Server;
 - Enjoy.
 
-Hint: To see keys, run script without "-Key" option: _powershell -File C:\zabbix\scripts\wsus_miner.ps1 -Action "Get" -Object "**Object**"_
+Hints:
+- To see keys, run script without "-Key" option: _powershell -File C:\zabbix\scripts\wsus_miner.ps1 -Action "Get" -Object "**Object**"_
+- If you use non-english (for example Russian Cyrillic) symbols into Computer Group's names and want to get correct UTF-8 on Zabbix Server side, then you must add _-consoleCP **your_native_codepage**_ parameter to command line. For example to convert from Russian Cyrillic codepage (CP866), use _powershell -File C:\zabbix\scripts\wsus_miner.ps1 -Action "$1" -Object "$2" -Key "$3" -Id "$4" -consoleCP CP866_.
 
-Beware: connection to WSUS is slow, don't use small timeouts with Data Items.
+Beware: frequent connections to WSUS may be nuke it and yours requests will be processeed slowly. To avoid it - don't use small update intervals with Data Items and disable unused.
 
--- wsus_miner.ps1
+++ wsus_miner.ps1
@@ -6,66 +6,139 @@
 Param (
 [string]$Action,
 [string]$Object,
-[string]$Key
+[string]$Key,
+[string]$Id,
+[string]$consoleCP
 )
 
-Function Convert-ToUnixTime { 
-  Param ($dateTime);
-  (New-TimeSpan -Start (Get-Date -Date "01/01/1970") -End $dateTime).TotalSeconds;
+function ConvertTo-Encoding ([string]$From, [string]$To){  
+    Begin{  
+        $encFrom = [System.Text.Encoding]::GetEncoding($from)  
+        $encTo = [System.Text.Encoding]::GetEncoding($to)  
+    }  
+    Process{  
+        $bytes = $encTo.GetBytes($_)  
+        $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)  
+        $encTo.GetString($bytes)  
+    }  
 }
 
-Function Get-WSUSLastSynchronizationInfo { 
-  Param ($key, $WSUS);
-  $Result = ($WSUS.GetSubscription()).GetLastSynchronizationInfo();
-
-  if ($Key) { 
-    $Result = $Result.$Key; 
-    switch ($key) {
-       ('StartTime') {
-          Convert-ToUnixTime -DateTime $Result.DateTime;
-       }
-       default  { $Result; }
+Function ConvertTo-UnixTime { 
+    Begin{  
+        $StartDate = Get-Date -Date "01/01/1970";
     }  
-  } else {
-    $Result;
+    Process{  
+       (New-TimeSpan -Start $StartDate -End $_).TotalSeconds;
+    }  
+}
+
+
+Function Make-JSON {
+  Param ([PSObject]$InObject, [array]$ObjectProperties, [boolean]$Pretty);
+  # Pretty json contain spaces, tabs and new-lines
+  if ($Pretty) { $CRLF = "`n"; $Tab = "    "; $Space = " "; } else {$CRLF = $Tab = $Space = "";}
+  # Init JSON-string $InObject
+  $Result = "{$CRLF$Space`"data`":[$CRLF";
+  # Take each Item from $InObject, get Properties that equal $ObjectProperties items and make JSON from its
+  $k = 0;
+  ForEach ($Object in $InObject) {$k++;
+     $Result += "$Tab$Tab{$Space";
+     # Process properties. No comma printed after last item
+     $ObjectProperties | % {$i = 0} {$i++; $Result += "`"{#$_}`":$Space`""+$Object.$_+"`""+(&{if ($i -lt $ObjectProperties.Count) {",$Space"} })}
+     # No comma printed after last string
+     $Result += " }"+(&{if ($k -lt $InObject.Count) {",$Space"} })+"$CRLF";
   }
+  # Finalize and return JSON
+  "$Result$Space]$CRLF}";
+}
+
+
+Function Get-WSUSLastSynchronizationInfo ([PSObject]$WSUS, [string]$key) { 
+  $Result = ($WSUS.GetSubscription()).GetLastSynchronizationInfo();
+  # check for Key existience
+  switch ($key) {
+     # Key is 'StartTime'. Need to make Unix timestamp from DateTime object and replace 'StartTime' Property with it
+     ('StartTime') { New-Object PSObject -Property @{ $key = ($Result.$key.DateTime | ConvertTo-UnixTime) }}; 
+     # Otherwise - just return object
+     default {$Result;}
+  }  
   
 }
 
-[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null
+Function Get-WSUSTotalSummaryPerGroupTarget ([PSObject]$WSUS, [string]$key, [string]$Id) { 
+  # Take all computers into Group with specific ID
+  $ComputerTargets = ($objWSUS.GetComputerTargetGroup($Id)).GetTotalSummaryPerComputerTarget()
+
+  # Analyzing Key and count how much computers present into collection from selection 
+  switch ($key) {
+     ('ComputerTargetsWithUpdateErrorsCount') {
+         # Select and count all computers with property FailedCount > 0
+         $Result = (($ComputerTargets | Where { $_.FailedCount -gt 0 }) | measure).Count
+     }
+     ('ComputerTargetsNeedingUpdatesCount') {
+        $Result = (($ComputerTargets | Where { ($_.NotInstalledCount -gt 0 -Or $_.DownloadedCount -gt 0 -Or $_.InstalledPendingRebootCount -gt 0) -And $_.FailedCount -le 0}) | measure).Count
+     }                    
+     ('ComputersUpToDateCount') {
+         # why .Count without `measure` not give 1 with UnknownCount = 0?  ("only warpig in select" case)
+         $Result = (($ComputerTargets | Where { $_.UnknownCount -eq 0 -And $_.NotInstalledCount -eq 0 -And $_.DownloadedCount -le 0 -And $_.InstalledPendingRebootCount -le 0 -And $_.FailedCount -le 0 }) | measure).Count   
+     }                    
+     ('ComputerTargetsUnknownCount') {
+         $Result = (($ComputerTargets | Where { $_.UnknownCount -gt 0 -And $_.NotInstalledCount -le 0 -And $_.DownloadedCount -le 0 -And $_.InstalledPendingRebootCount -le 0 -And $_.FailedCount -le 0 }) | measure).Count
+     }               
+  }
+
+  # If no computers is selected - collection will be empty and $Result will be undefined. Need to make undefined to 0
+  $Result = (&{if ($Result) { $Result } else { 0 }});
+  # return new object with property that named as key and contain number of computers into selection
+  New-Object PSObject -Property @{ $key = $Result }; 
+}
+
+
+[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | Out-Null
 # connect on Local WSUS
 $objWSUS = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer();
-$needProcessKey = $True;
 
-switch ($Object) {
-    ('Status') {
-        $Result = $objWSUS.GetStatus();
-    }
+# if needProcess is False - $Result is not need to convert to string and etc 
+$needProcess = $True;
 
-    ('Database') {
-        $Result = $objWSUS.GetDatabaseConfiguration();
-    }
-
-    ('Info') {
-        $Result = $objWSUS;
-    }
-
-    ('LastSynchronization') {
-        $Result = Get-WSUSLastSynchronizationInfo -Key $key -WSUS $objWSUS;
-    }
-
-    ('SynchronizationProcess') {
-          $needProcessKey = $False;
-          $Result = ($objWSUS.GetSubscription()).GetSynchronizationStatus();
-    }
-
-    default  { 
-          $needProcessKey = $False;
-          $Result = "Incorrect object: '$Object'"; 
-    }
+switch ($Action) {
+     #
+     # Discovery given object, make json for zabbix
+     #
+     ('Discovery') {
+         $needProcess = $False;
+         switch ($Object) {
+            ('ComputerGroup')                  { $ObjectProperties = @("NAME", "ID"); $InObject = $objWSUS.GetComputerTargetGroups(); }
+         }
+         $Result = Make-JSON -InObject $InObject -ObjectProperties $ObjectProperties -Pretty $True;
+     }
+     #
+     # Get metrics from object (real or virtual)
+     #
+     ('Get') {
+        switch ($Object) {
+            ('Info')                   { $Result = $objWSUS; }
+            ('Status')                 { $Result = $objWSUS.GetStatus(); }
+            ('Database')               { $Result = $objWSUS.GetDatabaseConfiguration(); }
+            ('Configuration')          { $Result = $objWSUS.GetConfiguration(); }
+            ('ComputerGroup')          { $Result = Get-WSUSTotalSummaryPerGroupTarget -WSUS $objWSUS -Key $key -Id $Id; }
+            ('LastSynchronization')    { $Result = Get-WSUSLastSynchronizationInfo -WSUS $objWSUS -Key $key ; }
+            ('SynchronizationProcess') { $needProcess = $False; $Result = ($objWSUS.GetSubscription()).GetSynchronizationStatus(); }
+            default                    { $needProcess = $False; $Result = "Incorrect object: '$Object'";}
+        }  
+        if ($needProcess -And $Key) { $Result = ($Result.$Key).ToString(); }
+     }
+     #
+     # Error
+     #
+     default  { $Result = "Incorrect action: '$Action'"; }
 }  
 
-if ($needProcessKey -And $Key) { $Result=($Result.$Key).ToString(); }
+# Normalize String object
 $Result = ($Result | Out-String).trim();
+
+# Convert String to UTF-8 if need (For Zabbix LLD-JSON with Cyrillic for example)
+if ($consoleCP) { $Result = $Result | ConvertTo-Encoding -From $consoleCP -To UTF-8 }
+
 Write-Host $Result;
 
